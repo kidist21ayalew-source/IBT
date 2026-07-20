9740678f9c3b373caffd21f9502c0b6d1e7227b6
kidist21ayalew-source <kidist21ayalew@gmail.com>
2026-07-20 11:13:47 -0700

    complete quation No3

--- Module-01/Day3/practice.py
+++ Module-01/Day3/practice.py
@@ -1,7 +1,21 @@
 #Quation No 1
-city =["Addis Abbeba","Adama", 'Disse','Hawassa', "Addis Abbeba", "Disse"]
+# city =["Addis Abbeba","Adama", 'Disse','Hawassa', "Addis Abbeba", "Disse"]
 
-print(set(city))
+# print(set(city))
+
+
+
+
+
+
+#quation No 3
+prices =[100, 250, 400, 80]
+with_tax =[(price + (price * 0.15)) for price in prices]
+print(with_tax)
+
+
+
+
 
 
 

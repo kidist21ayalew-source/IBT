29a0bab058b11c82edc77a72e3e7149137c6d90c
kidist21ayalew-source <kidist21ayalew@gmail.com>
2026-07-20 12:21:38 -0700

    Complete quation No3

--- Module-01/Day3/practice.py
+++ Module-01/Day3/practice.py
@@ -5,12 +5,21 @@
 
 
 #Quation N2
-grocery ={"Onion": 100, "Potato":30, "Avocado": 150, "Mango":220, "Appel": 850}
+#grocery ={"Onion": 100, "Potato":30, "Avocado": 150, "Mango":220, "Appel": 850}
 
-for item, price in grocery. items() :
-    print(f"{item}:{price} ETB")
+#for item, price in grocery. items() :
+ #   print(f"{item}:{price} ETB")
 
 
+#Quation N3
+price = [100, 250, 400, 80]
+
+with_tax = [price +(price * 0.15) for price in price]
+print(with_tax)
+
+
+
+#Quation No4
 
 
 

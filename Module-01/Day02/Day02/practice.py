e1ee7e386fc879a5c4eaa3b74cb90c3d066cc69b
kidist21ayalew-source <kidist21ayalew@gmail.com>
2026-07-21 10:24:41 -0700

    compleat quation No 2 day 2 practice

--- Module-01/Day02/Day02/practice.py
+++ Module-01/Day02/Day02/practice.py
@@ -1,9 +1,17 @@
 # quation No 1
-temp = float(input("Enter a temperature in °C: "))
+# temp = float(input("Enter a temperature in °C: "))
 
-if temp < 15:
-    print("cold")
-elif 15 <= temp <= 28:
-    print("warm")
-else:
-    print("hot")
+# if temp < 15:
+#     print("cold")
+# elif 15 <= temp <= 28:
+#     print("warm")
+# else:
+#     print("hot")
+    
+
+#Qation No 2
+
+for i in range(1, 11):
+  print(f"Receipt#N {i}")
+
+ 

efe54274590cfd240d31acd0854ef7c3ce19777b
kidist21ayalew-source <kidist21ayalew@gmail.com>
2026-08-13 00:25:48 -0700

    compleat tailebirr project

---
+++ module02/day18/tailebirr-project/transaction.js
@@ -0,0 +1,24 @@
+const transactions = [
+    {
+        id:1, 
+        customer: "Kidist",
+        amount: 500,
+        type: "credit"
+    },
+    
+    {
+        id:2,
+        customer: "Dage",
+        amount:200,
+        type: "debit"
+    }
+    {
+        id:3,
+        customer:"Hasset",
+        amount:1000,
+        type:"debit"
+    }
+
+    
+];
+export {transactions};
